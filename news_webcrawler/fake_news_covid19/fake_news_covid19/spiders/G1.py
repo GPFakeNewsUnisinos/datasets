e969c7cf8a7c3d8e4dcd18e0e1d97fd94d031665
crimachado <46095736+crimachado@users.noreply.github.com>
2021-07-06 07:31:37 -0300

    adding changes on g1 spider

--- news_webcrawler/fake_news_covid19/fake_news_covid19/spiders/G1.py
+++ news_webcrawler/fake_news_covid19/fake_news_covid19/spiders/G1.py
@@ -1,27 +1,33 @@
 import scrapy
-import urlparse
+import csv
 
-class News(scrapy.Item):
-    link = scrapy.Field()
-    title = scrapy.Field()
-    content = scrapy.Field()
-    date = scrapy.Field()
+from fake_news_covid19.items import NewsItem
+
     
 #TODO analyse all urls and create a generic function to attend all if its possible
 
 class G1Spider(scrapy.Spider):
     name = 'G1'
     allowed_domains = ['g1.globo.com']
-    start_urls = ['http://g1.globo.com/fato-ou-fake/'+'?sort=covid']
+    start_urls = ['http://g1.globo.com/fato-ou-fake/coronavirus'+'?sort=covid',
+                  'http://g1.globo.com/fato-ou-fake/coronavirus'+'?sort=vacina']
 
+   
+    
     def parse(self, response):
+        f= open('fake_news_covid19.csv', 'w', newline='', encoding='utf-8')
+        w = csv.writer(f)
         self.log('ACESSANDO URL: %s' % response.url)
-        build_full_url = lambda link: urlparse.urljoin(response.url, link)
-        for qsel in response.css("#fato-ou-fake> div"):
-            it = News()
-            it['link'] = build_full_url(
-                qsel.css('.summary h3 > a').xpath('@href')[0].extract())
-            it['title'] = qsel.css('.summary h3 &amp;amp;amp;gt; a::text')[0].extract()
-            it['content'] = qsel.css('a.post-tag::text').extract()
-            it['date'] = qsel.css('div.excerpt::text')[0].extract()
-            yield it
+        for notice in response.css("div.feed-post"):
+            link= notice.css('a.feed-post-link').xpath('@href')[0].extract()
+            title = notice.css('div.feed-post-body-title a::text').extract()
+           # author = notice.css()
+            content = notice.css('div.feed-post-body-resumo::text').extract()
+            #date = notice.css('div.excerpt::text')[0].extract()            
+            mynews = NewsItem(link = link, title =  title, content = content)
+            print(mynews)
+        
+        yield mynews
+        w.writerow(mynews)
+        w.close()
+    
